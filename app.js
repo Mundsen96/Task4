85b14bcc3ab40e87462af05954f8ce5ffa0d1d30
Sebastian Młotkowski <sebamlot142@gmail.com>
2022-09-26 13:52:09 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoConnect = require('./util/database').mongoConnect;
+const adminController = require('./controllers/admin');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const isAuth = require('./middleware/is-Auth');
+
+
+const app = express();
+const store = new MongoDBStore({
+  uri: 'mongodb+srv://Sebastian:<password>@task4cluster.7hfe3rj.mongodb.net/users',
+  collection: 'sessions'
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+
+require('dotenv').config();
+port = parseInt(process.env.PORT);
+
+app.listen(port,_=>console.log(`The server is listening on port ${port}`) );
+
+app.get('/users',isAuth , adminController.getUsers);
+
+app.post('/register', adminController.postAddUser);
+
+app.post('/login', adminController.logIn);
+app.post('/users', adminController.updateUsers);
+// app.get('/auth', isAuth);
+
+mongoConnect(() => {
+  app.listen(3002);
+});
+
+
+``
